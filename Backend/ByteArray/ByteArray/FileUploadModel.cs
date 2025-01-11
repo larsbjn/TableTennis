efaa529153836f2300fc0649c2c38a9c4fc18173
Lars Jørgensen <lars.b.jorgensen@visma.com>
2025-01-11 21:24:44 +0100

    Remove test project

--- Backend/ByteArray/ByteArray/FileUploadModel.cs
+++
@@ -1,7 +0,0 @@
-namespace ByteArray;
-public class FileUploadModel
-{
-    public byte[] Data { get; set; }  // Base64 string will be automatically converted to byte[] by ASP.NET Core
-    public string FileName { get; set; }
-    public string Description { get; set; }
-}
